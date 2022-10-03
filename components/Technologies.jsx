5e167829a45d3cd2d2a5e706a1a4bdf281c3497f
EdgardoAVS <edgardoovargass@gmail.com>
2022-10-03 13:36:40 -0500

    Deleted images

--- components/Technologies.jsx
+++ components/Technologies.jsx
@@ -12,7 +12,7 @@ import NodeJS from "../public/assets/technologies/node.png";
 
 export const Technologies = () => {
   return (
-    <div id="skills" className="w-full lg:h-screen p-2">
+    <div id="technologies" className="w-full lg:h-screen p-2">
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <p className="text-xl tracking-widest uppercase text-[#5651e5]">
           Technologies
